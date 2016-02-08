fa8c5b7fc13cd7c0a827c0e22aa45aed7f6c1ef9
Michael Gutierrez <michael.gutierrez2@marist.edu>
2016-02-07 20:48:15 -0500

    Created Java file for text adventure game.

---
+++ HouseOfQuestions/src/HouseOfQuestions.java
@@ -0,0 +1,19 @@
+/**
+ * @author Michael Gutierrez
+ * CMPT 220L-114
+ * Professor Johnson
+ * 9 February 2016
+ *
+ */
+
+public class HouseOfQuestions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+
+	}
+
+}
