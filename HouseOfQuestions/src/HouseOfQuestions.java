282d180116edcb3af31c98fd4bfe30121554d80e
Michael Gutierrez <michael.gutierrez2@marist.edu>
2016-02-08 21:25:54 -0500

    Added basic loop taking in user input.

--- HouseOfQuestions/src/HouseOfQuestions.java
+++ HouseOfQuestions/src/HouseOfQuestions.java
@@ -23,8 +23,37 @@ public class HouseOfQuestions {
 		System.out.println("\n"+"House of Questions");
 		System.out.println("------------------"+"\n");
 		System.out.println("You wake up to find yourself inside of the "+
-			"'House of Questions'\n"+"Nothing else to do but explore...");
+			"'House of Questions'\n"+"Nothing else to do but explore...\n");
 		
+		
+		while (true) {
+			// User input that is case-insensitive
+			System.out.print("Where should I go?: ");
+			userInput = inputSource.nextLine().toUpperCase();
+			
+			// Game loop until user quits
+			if (userInput.equals("N")) {
+				direction = "North";
+			} else if (userInput.equals("S")) {
+				direction = "South";
+			} else if (userInput.equals("E")) {
+				direction = "East";
+			} else if (userInput.equals("W")) {
+				direction = "West";
+			} else if (userInput.equals("Q")) {
+				break;
+			} else {
+				System.out.println("Not a valid command!\n");
+				continue;
+			}
+			
+			System.out.println("You moved " + direction + ".\n");
+		}
+		
+		System.out.println("Quitting game... ;(");
+		inputSource.close();
+	
+	
 	}
 
 	
