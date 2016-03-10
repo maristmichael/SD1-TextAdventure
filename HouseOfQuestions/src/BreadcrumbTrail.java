20a3ed539d15d5288d573e2189d1c172fe4d1a38
Michael Gutierrez <michael.gutierrez2@marist.edu>
2016-03-10 15:38:52 -0500

    Added dropCrumb & pickupCrumb methods to BreadcrumbTrail

--- HouseOfQuestions/src/BreadcrumbTrail.java
+++ HouseOfQuestions/src/BreadcrumbTrail.java
@@ -7,6 +7,17 @@ public class BreadcrumbTrail implements BreadcrumbStack {
 		this.top = null;
 	}
 	
+	@Override
+	public void dropCrumb(Object x){
+		Breadcrumb newCrumb = new Breadcrumb(x, this.top);
+		this.top = newCrumb;
+	}
+	
+	@Override
+	public void pickupCrumb(){
+		this.top = this.top.link;
+	}
+	
 	class Breadcrumb {
 		Object data;
 		Breadcrumb link;
