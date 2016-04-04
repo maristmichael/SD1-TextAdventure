210373fd9f1f5f988cc7aff0c164f89513985896
Michael Gutierrez <michael.gutierrez2@marist.edu>
2016-04-04 10:39:14 -0400

    Item Class now has two new attributes: isDiscovered and value

--- HouseOfQuestions/src/Item.java
+++ HouseOfQuestions/src/Item.java
@@ -2,10 +2,14 @@
 public class Item {
 	String name;
 	String descrip;
+	boolean isDiscovered;
+	int value;
 	
 	public Item(String name, String descrip){
 		this.name = name;
 		this.descrip = descrip;
+		this.value = 5;
+		this.isDiscovered = false;
 	};
 	
 	@Override
