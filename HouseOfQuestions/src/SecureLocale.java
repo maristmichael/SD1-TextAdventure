d65dc33a714dfbab4160fdfae2a59762eaf5e849
Michael Gutierrez <michael.gutierrez2@marist.edu>
2016-04-22 00:55:05 -0400

    Created SecureLocale class which extends Locale class

---
+++ HouseOfQuestions/src/SecureLocale.java
@@ -0,0 +1,19 @@
+
+public class SecureLocale extends Locale {
+	Item requiredItem;
+	
+	public SecureLocale(String name, String description, Item requiredItem) {
+		super(name, description);
+		this.requiredItem = requiredItem;
+	}
+	
+	public static boolean canEnter(Player user, Item requiredItem) {
+		for (int i = 0; i < user.inventory.size(); i++) {
+			if (user.inventory.contains(requiredItem)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
